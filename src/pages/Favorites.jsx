5bdfc9bf21f306e50c237ec8570e30219e29ca02
Noor Chopra <241230035@nitdelhi.ac.in>
2026-07-23 19:01:30 +0530

    klo

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -175,12 +175,13 @@ const Favorites = () => {
 
           <div className="
             grid
-            grid-cols-1
+            grid-cols-2
             sm:grid-cols-2
             md:grid-cols-3
             lg:grid-cols-4
             xl:grid-cols-5
-            gap-6
+            gap-3
+            sm:gap-6
           ">
 
 
--- src/pages/RecentlyPlayed.jsx
+++ src/pages/RecentlyPlayed.jsx
@@ -97,12 +97,13 @@ const user = {
         <div
           className="
             grid
-            grid-cols-1
+            grid-cols-2
             sm:grid-cols-2
             md:grid-cols-3
             lg:grid-cols-4
             xl:grid-cols-5
-            gap-6
+            gap-3
+            sm:gap-6
           "
         >
           {songs.map((song, i) => (
