ff3920d870ee8b28c0bc6f67d524726990988f87
Noor Chopra <241230035@nitdelhi.ac.in>
2026-07-23 15:10:34 +0530

    Fix duplicate Cloudinary config

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,17 +1,8 @@
 const cloudinary = require("cloudinary").v2;
-const cloudinary = require("cloudinary").v2;
 
 console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
 console.log("API Key:", process.env.CLOUDINARY_API_KEY);
-console.log("Secret:", !!process.env.CLOUDINARY_API_SECRET);
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-module.exports = cloudinary;
+console.log("Secret Exists:", !!process.env.CLOUDINARY_API_SECRET);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
